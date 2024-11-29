76adf70765270e2ceb4244cda70b389300077e3a
Kalfontein Cruz <cruz.310690@caloocan.sti.edu.ph>
2024-11-30 04:12:12 +0800

    - added fully functional create user

--- AdminDashboard.Designer.cs
+++ AdminDashboard.Designer.cs
@@ -861,7 +861,7 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(492, 401);
+            this.button1.Location = new System.Drawing.Point(38, 466);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(190, 35);
             this.button1.TabIndex = 33;
@@ -874,7 +874,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.checkBox1.AutoSize = true;
             this.checkBox1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBox1.Location = new System.Drawing.Point(825, 151);
+            this.checkBox1.Location = new System.Drawing.Point(821, 305);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(15, 14);
             this.checkBox1.TabIndex = 32;
@@ -888,7 +888,7 @@
             this.updateAccount.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.updateAccount.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.updateAccount.ForeColor = System.Drawing.Color.White;
-            this.updateAccount.Location = new System.Drawing.Point(711, 401);
+            this.updateAccount.Location = new System.Drawing.Point(257, 466);
             this.updateAccount.Name = "updateAccount";
             this.updateAccount.Size = new System.Drawing.Size(190, 35);
             this.updateAccount.TabIndex = 31;
@@ -900,7 +900,7 @@
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(573, 231);
+            this.textBox1.Location = new System.Drawing.Point(569, 385);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.PasswordChar = '*';
@@ -912,7 +912,7 @@
             this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(573, 144);
+            this.textBox2.Location = new System.Drawing.Point(569, 298);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.PasswordChar = '*';
@@ -975,7 +975,7 @@
             this.comboBox1.Items.AddRange(new object[] {
             "Manager",
             "User"});
-            this.comboBox1.Location = new System.Drawing.Point(573, 321);
+            this.comboBox1.Location = new System.Drawing.Point(569, 475);
             this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(275, 28);
@@ -988,7 +988,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Sienna;
-            this.label2.Location = new System.Drawing.Point(573, 285);
+            this.label2.Location = new System.Drawing.Point(569, 439);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(109, 24);
@@ -1002,7 +1002,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Sienna;
-            this.label3.Location = new System.Drawing.Point(573, 190);
+            this.label3.Location = new System.Drawing.Point(569, 344);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(208, 24);
@@ -1016,7 +1016,7 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.Sienna;
-            this.label6.Location = new System.Drawing.Point(573, 98);
+            this.label6.Location = new System.Drawing.Point(569, 252);
             this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(121, 24);
